f0e81b4dd60e8757d57c845f1d7c4818097bc047
MarkYen <l6071255@gmail.com>
2022-09-15 09:58:41 +0800

    version 3

--- backend/app.js
+++ backend/app.js
@@ -47,6 +47,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3001);
+app.listen(3001,()=>{
+  console.log("Server is running at 3001!");
+});
 
 // module.exports = app;
